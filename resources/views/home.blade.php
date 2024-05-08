c9ec83138c9be0616116aba72dc17ae324f096fb
YasirArafat <yeasherarafath@gmail.com>
2024-05-09 03:39:50 +0600

    late submit and complete user panel (orginal is: dev.0.0.1)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,53 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-body bg-info text-center">
+                                    <h2>Balance</h2>
+                                    <h3>{{ $data['user']->balance }} BDT</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-header">
+                                    <div class="card-title">All Transaction</div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Name</th>
+                                                    <th>Email</th>
+                                                    <th>Amount</th>
+                                                    <th>Fee</th>
+                                                    <th>Date</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($data['transactions'] as $transaction)
+                                                <tr>
+                                                    <th>{{ $loop->iteration }}</th>
+                                                    <th>{{ $transaction->user->name }}</th>
+                                                    <th>{{ $transaction->user->email }}</th>
+                                                    <th>{{ $transaction->amount }}</th>
+                                                    <th>{{ $transaction->fee }}</th>
+                                                    <th>{{ $transaction->date }}</th>
+                                                </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- {{ __('You are logged in!') }} --}}
                 </div>
             </div>
         </div>
